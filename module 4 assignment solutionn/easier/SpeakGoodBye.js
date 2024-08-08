a84c92f3f8ed27e8017f59279282a0977eb6b3f6
eman654 <emmanuelmintesinot@gmail.com>
2024-08-08 20:01:44 +0300

    Add files via upload

---
+++ module 4 assignment solutionn/easier/SpeakGoodBye.js
@@ -0,0 +1,10 @@
+(function (window) {
+  var speakWord = "Goodbye";
+  var byeSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.byeSpeaker = byeSpeaker;
+})(window);
+window()
